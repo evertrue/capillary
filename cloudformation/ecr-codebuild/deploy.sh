6251cf7c8a8c880e0206d88b32090724c4dab7cf
Doug Youch <dougyouch@gmail.com>
2022-01-29 16:18:37 -0500

    work around container for setting up datadog

--- cloudformation/ecr-codebuild/deploy.sh
+++ cloudformation/ecr-codebuild/deploy.sh
@@ -7,6 +7,7 @@ ToolsAccountProfile=${ToolsAccountProfile:-evertruetools}
 
 StackName=et-capillary
 ECRRepositoryName=evertrue/et-capillary
+ECRRepositoryDatadogName=evertrue/et-capillary-datadog
 GitHubProjectName=capillary
 
-aws cloudformation deploy --stack-name $StackName --template-file ecr-codebuild.yaml --parameter-overrides ECRRepositoryName=$ECRRepositoryName GitHubProjectName=$GitHubProjectName --profile $ToolsAccountProfile
+aws cloudformation deploy --stack-name $StackName --template-file ecr-codebuild.yaml --parameter-overrides ECRRepositoryName=$ECRRepositoryName ECRRepositoryDatadogName=$ECRRepositoryDatadogName GitHubProjectName=$GitHubProjectName --profile $ToolsAccountProfile
--- cloudformation/ecs/deploy.sh
+++ cloudformation/ecs/deploy.sh
@@ -7,10 +7,12 @@ Host="capillary.evertrue.com"
 ZookeeperHosts="prod-zookeeper-1c.priv.evertrue.com:2181,prod-zookeeper-1d.priv.evertrue.com:2181,prod-zookeeper-1b.priv.evertrue.com:2181"
 AccountProfile="evertrue${EnvType}"
 StackName="ecs-cluster-${EnvType}-DevOpsServices-Capillary"
+DATADOG=""
 
 if [ "$EnvType" = "stage" ]; then
   Host="stage-capillary.evertrue.com"
   ZookeeperHosts="stage-zookeeper-1c.priv.evertrue.com:2181,stage-zookeeper-1d.priv.evertrue.com:2181,stage-zookeeper-1b.priv.evertrue.com:2181"
+  DATADOG=""
 fi
 
-aws cloudformation deploy --stack-name $StackName --template-file ecs-service.yaml --parameter-overrides EnvType=$EnvType Host=$Host ZookeeperHosts=$ZookeeperHosts --profile $AccountProfile
+aws cloudformation deploy --stack-name $StackName --template-file ecs-service.yaml --parameter-overrides EnvType=$EnvType Host=$Host ZookeeperHosts=$ZookeeperHosts DATADOG=$DATADOG --profile $AccountProfile

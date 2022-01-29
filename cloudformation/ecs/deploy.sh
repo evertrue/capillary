38becd669647cb996a2f46c74b7eb4b92f858ef2
Doug Youch <dougyouch@gmail.com>
2022-01-29 17:05:44 -0500

    configure DATADOG env

--- cloudformation/ecs/deploy.sh
+++ cloudformation/ecs/deploy.sh
@@ -7,12 +7,12 @@ Host="capillary.evertrue.com"
 ZookeeperHosts="prod-zookeeper-1c.priv.evertrue.com:2181,prod-zookeeper-1d.priv.evertrue.com:2181,prod-zookeeper-1b.priv.evertrue.com:2181"
 AccountProfile="evertrue${EnvType}"
 StackName="ecs-cluster-${EnvType}-DevOpsServices-Capillary"
-DATADOG=""
+Datadog="-Dcapillary.metrics.datadog.apiKey=cd20be2b109bd4eb7413f071afb01a8d"
 
 if [ "$EnvType" = "stage" ]; then
   Host="stage-capillary.evertrue.com"
   ZookeeperHosts="stage-zookeeper-1c.priv.evertrue.com:2181,stage-zookeeper-1d.priv.evertrue.com:2181,stage-zookeeper-1b.priv.evertrue.com:2181"
-  DATADOG=""
+  Datadog=""
 fi
 
-aws cloudformation deploy --stack-name $StackName --template-file ecs-service.yaml --parameter-overrides EnvType=$EnvType Host=$Host ZookeeperHosts=$ZookeeperHosts DATADOG=$DATADOG --profile $AccountProfile
+aws cloudformation deploy --stack-name $StackName --template-file ecs-service.yaml --parameter-overrides EnvType=$EnvType Host=$Host ZookeeperHosts=$ZookeeperHosts Datadog=$Datadog --profile $AccountProfile
--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DATADOG=$(wget -q -O - http://169.254.169.254/latest/meta-data/local-ipv4)
-
-/capillary/capillary-1.2/bin/capillary -Dcapillary.zookeepers=${ZOOKEEPER} -Dcapillary.kafka.zkroot="/kafka" -Dcapillary.storm.zkroot="/storm-kafka" ${DATADOG}
